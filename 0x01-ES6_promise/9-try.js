864a0696009227d24d06b58cc95aa8579e8d54f8
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-06-26 11:29:08 -0700

    try/catch and throw an Error

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+export default function guardrail(mathFunction()) {
+  const queue = [];
+  let results;
+  try {
+    results = mathFunction();
+  } catch (error) {
+    results = `Error: ${error.message}`;
+  }
+  queue.push(results);
+  queue.push("Guardrail was processed");
+  return queue;
+}
