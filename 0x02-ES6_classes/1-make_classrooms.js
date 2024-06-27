8554b3c2f74b8ac4c3c40df9362c5c055f66effe
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-06-27 04:27:16 -0700

    classroom class

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,9 @@
-import ClassRoom from './0-classroom.js';
-
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   return [
-    new classRoom(19),
-    new classRoom(20),
-    new classRoom(34),
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
   ];
 }
