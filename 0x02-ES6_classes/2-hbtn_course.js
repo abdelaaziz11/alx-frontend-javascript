a672ec3534feb5685d8f0fcec0dbef0cc0dfdff0
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-06-27 04:48:05 -0700

    A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,4 +1,4 @@
-export default class HolbertonCourse() {
+export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
@@ -7,6 +7,7 @@ export default class HolbertonCourse() {
     } else if (!Array.isArray(students)) {
       throw new TypeError('Students must be an array of strings');
     }
+
     this._name = name;
     this._length = length;
     this._students = students;
@@ -15,18 +16,22 @@ export default class HolbertonCourse() {
   get name() {
     return this._name;
   }
+
   get length() {
     return this._length;
   }
+
   get students() {
-    this._students;
+    return this._students;
   }
+
   set name(name) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = name;
   }
+
   set length(length) {
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
