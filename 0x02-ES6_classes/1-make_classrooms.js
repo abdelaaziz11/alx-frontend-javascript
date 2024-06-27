5076f749f00620ed4ab3854b43129bd6d0dc4cf0
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-06-27 04:18:22 -0700

    classroom class

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,6 @@
 import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-  const room1 = classRoom(19);
-  const room2 = classRoom(20);
-  const room1 = classRoom(34);
   return [
     new classRoom(19),
     new classRoom(20),
