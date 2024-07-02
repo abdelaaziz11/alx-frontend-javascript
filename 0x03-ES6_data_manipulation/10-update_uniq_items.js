b0856a2b0fb580fd9b7d1b5e3d6a2d566af64ceb
abdelaaziz11 <140088735+abdelaaziz11@users.noreply.github.com>
2024-07-02 18:00:44 +0100

    update unique items

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,10 @@
-const updateUniqueItems = (map) => {
-  if (!(map instanceof Map)) throw new Error('Cannot process');
-
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
   map.forEach((value, key) => {
-    if (value === 1) map.set(key, 100);
+    if (value === 1) {
+      map.set(key, 100);
+    }
   });
-};
-
-export default updateUniqueItems;
+}
