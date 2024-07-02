c501f6480c076d5c9287c3351691b59a7f645e08
abdelaaziz11 <140088735+abdelaaziz11@users.noreply.github.com>
2024-07-02 12:23:50 +0100

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,19 @@
 export default function cleanSet(set, startString) {
-  if (!startString) {
+  if (
+    typeof set !== 'object'
+    || typeof startString !== 'string'
+    || startString.length === 0
+  ) {
     return '';
   }
+
   let res = [];
+
   set.forEach((value) => {
     if (value.startsWith(startString)) {
-      res.push(value.substring(startString.length));
+      res.push(value.slice(startString.length));
     }
   });
+
   return res.join('-');
 }
