7dd9cfde6e8f96c97ee264fc558a6f4f85406aed
abdelaaziz11 <140088735+abdelaaziz11@users.noreply.github.com>
2024-07-02 02:39:16 +0100

    get students by location

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getStudentIdsSum(students) {
-    return students.reduce((acc, current) => acc + current.id, 0);
+  return students.reduce((acc, current) => acc + current.id, 0);
 }
